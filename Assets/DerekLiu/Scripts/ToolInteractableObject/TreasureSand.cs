e37ced6b70d345c4765e6c09473e0af1c87c7ae7
LiuDingxin <2230457162@qq.com>
2025-04-16 11:16:59 +0800

    导入土坑素材

---
+++ Assets/DerekLiu/Scripts/ToolInteractableObject/TreasureSand.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace DerekLiu.Scripts
+{
+    public class TreasureSand : TreasureSandBase
+    {
+        public Texture[] textures;
+        
+        public override void OnToolInteract(DerekDigGameTool tool)
+        {
+            if(tool.toolType == DerekDigGameToolType.Hammer)
+            {
+                material.SetTexture("_BaseMap", textures[currentInteractCount]);
+
+                currentInteractCount = Mathf.Min(currentInteractCount + 1, maxInteractCount);
+                if (currentInteractCount >= maxInteractCount)
+                {
+                    TreasureManager.instance.SpawnTreasure(transform.position, transform.rotation);
+                }
+            }
+        }
+    }
+}
--- Assets/DerekLiu/Scripts/TreasureManager/TreasureManager.cs
+++ Assets/DerekLiu/Scripts/TreasureManager/TreasureManager.cs
@@ -1,17 +1,65 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using PICOMR.Scripts.ResourcesLoader.ResourcesSO;
+using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace DerekLiu.Scripts
 {
     public class TreasureManager : SingletonMono<TreasureManager>
     {
-        public void SpawnTreasure()
+        public AssetSO assetSO;
+        
+        private List<TreasureBase> foundTreasures = new List<TreasureBase>();
+        private List<int> randomIndex = new List<int>();
+        private List<int> generatedIndex = new List<int>();
+        private void Start()
         {
-            
+            assetSO.InitAssetsData();
+
+            foreach (var prefabPair in assetSO.prefabDictionary)
+            {
+                randomIndex.Add((int)prefabPair.Key);
+            }
         }
 
-        public void DestroyTreasure(ITreasure treasure)
+        public GameObject SpawnTreasure(Vector3 position, Quaternion rotation)
         {
+            if (randomIndex.Count == 0)
+            {
+                return null;
+            }
             
+            int index = randomIndex[Random.Range(0, randomIndex.Count)];
+
+            var prefab = assetSO.prefabDictionary[(ulong)index];
+            
+            var go = Instantiate(prefab, position, rotation);
+            var t = go.GetComponent<TreasureBase>();
+            if (t != null)
+            {
+                t.id = index;
+                foundTreasures.Add(t);
+            }
+            else
+            {
+                Destroy(go);
+                return null;
+            }
+            
+            randomIndex.Remove(index);
+            generatedIndex.Add(index);
+            
+            return go;
+        }
+
+        public void DestroyTreasure(TreasureBase treasure)
+        {
+            randomIndex.Add(treasure.id);
+            generatedIndex.Remove(treasure.id);
+            foundTreasures.Remove(treasure);
+            
+            Destroy(treasure.gameObject);
         }
     }
 }
